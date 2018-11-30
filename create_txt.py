c72794090267fe6c94f1ac1f7c8ba10a9cdd007e
Evan Hubinger <evanjhub@gmail.com>
2018-11-29 21:00:52 -0800

    Revert create_txt

--- create_txt.py
+++ create_txt.py
@@ -5,7 +5,7 @@ for use in the cnn-cbir-benchmark repository.
 
 import os
 
-from score_retrieval.data import database_paths
+from score_retrieval.data import index_images
 
 
 default_index_fpath = os.path.join(os.path.dirname(__file__), "data", "minidataset.txt")
@@ -14,7 +14,7 @@ default_index_fpath = os.path.join(os.path.dirname(__file__), "data", "minidatas
 def write_index(index_fpath=default_index_fpath):
     """Write index of image paths to given index_fpath."""
     with open(index_fpath, "w") as index_file:
-        for path in database_paths:
+        for _, path in index_images():
             index_file.write("{}\n".format(path))
 
 
